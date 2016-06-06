43e584eb39ab0f3b385dd9c3c3b5264a10fc7080
Kent Fredric <kentfredric@gmail.com>
2016-06-07 07:59:08 +1200

    Add handling for installing dzil<6 on perl 5.14 when its a runtime/test dep.

--- install_deps.pl
+++ install_deps.pl
@@ -31,8 +31,10 @@ if ( env_is( 'TRAVIS_BRANCH', 'master' ) ) {
   # cpanm( @params, 'Devel::Confess' );
   # $ENV{PERL5OPT} = '-MDevel::Confess';
   if ( "$]" lt "5.014000" ) {
+    diag("\e[32mMaster + Perl <5.14\e[0m");
     cpanm( @params, 'Dist::Zilla~<6.0', 'Capture::Tiny', 'Pod::Weaver' );
-  } else {
+  }
+  else {
     cpanm( @params, 'Dist::Zilla', 'Capture::Tiny', 'Pod::Weaver' );
   }
   safe_exec( 'git', 'config', '--global', 'user.email', 'kentfredric+travisci@gmail.com' );
@@ -75,6 +77,48 @@ if ( env_is( 'TRAVIS_BRANCH', 'master' ) ) {
   }
 }
 else {
+  if ( "$]" lt "5.014000" ) {
+    diag("\e[32mScanning for Dzil runtime dep due to perl < 5.014\e[0m");
+    my $need_dzil;
+
+    if ( -e 'Makefile.PL' and open my $fh, '<', 'Makefile.PL' ) {
+      diag("\e[32mReading Makefile.PL\e[0m");
+      while ( my $line = <$fh> ) {
+        chomp $line;
+        next unless $line =~ /["']Dist::Zilla\S+["']\s*=>/;
+        diag("\e[33mDist::Zilla found: \e[0m$line");
+        $need_dzil = 1;
+        last;
+      }
+    }
+    elsif ( -e 'META.json' and open my $xfh, '<', 'META.json' ) {
+      diag("\e[32mReading META.json\e[0m");
+      while ( my $line = <$xfh> ) {
+        chomp $line;
+        if ( $line =~ /\A(\s*)["']prereqs["']\s*:\s*\{/ ) {
+          my $prereq_padding = "$1";
+          last if $line =~ /\A\Q$prereq_padding\E\}\s*\z/;
+
+          # Skip develop prereqs
+          if ( $line =~ /\A(\s+)["']develop["']\s*:\s*\{/ ) {
+            my $padding = "$1";
+            while ( my $line = <$xfh> ) {
+              last if $line =~ /\A\Q$padding\E\},?\s*\z/;
+            }
+            next;
+          }
+          if ( $line =~ /["']Dist::Zilla\S+["']\s*:\s*/ ) {
+            diag("\e[33mDist::Zilla found: \e[0m$line");
+            $need_dzil = 1;
+            last;
+          }
+        }
+      }
+    }
+    if ($need_dzil) {
+      cpanm( @params, 'Dist::Zilla~<6.0' );
+    }
+  }
   cpanm( @params, '--installdeps', '.' );
   if ( env_true('COVERAGE_TESTING') ) {
     cpanm( @params, 'Devel::Cover::Report::Coveralls' );
